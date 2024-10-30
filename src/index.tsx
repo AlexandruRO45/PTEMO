6569bb753c96dde1238adf27917d2e06d960c4d1
AlexandruRO45 <savaalexandru562@gmail.com>
2024-10-30 17:42:08 +0000

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./CarDecorationApp";
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
